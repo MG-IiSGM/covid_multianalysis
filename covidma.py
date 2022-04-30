efba83a2e3667c181bd003faad5516bdd52e57b3
daniel.penasutrilla98@gmail.com <daniel.penasutrilla98@gmail.com>
2022-04-30 11:11:35 +0200

    pruebas parallel bash5

--- covidma.py
+++ covidma.py
@@ -833,7 +833,7 @@ def map_sample(output, args, logger, r1_file, r2_file, sample_list_F, new_sample
         ########################################################
         coverage_stats(output, sample, output_markdup_trimmed_file, logger)
 
-def covidma(output, args, logger, r1, r2, sample_list_F, new_samples, group_name, reference, annotation):
+def covidma_pipeline(output, args, logger, r1, r2, sample_list_F, new_samples, group_name, reference, annotation):
 
     # Variables for parallelization
     nproc = multiprocessing.cpu_count()
@@ -990,4 +990,4 @@ set_folder_structure(output)
 r1, r2, sample_list_F, new_samples = obtain_reads(args, logger)
 
 # Run COVIDMA
-covidma(output, args, logger, r1, r2, sample_list_F, new_samples, group_name, reference, annotation)
+covidma_pipeline(output, args, logger, r1, r2, sample_list_F, new_samples, group_name, reference, annotation)
