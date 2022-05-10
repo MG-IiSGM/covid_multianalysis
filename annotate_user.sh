b581dbd0d93a35d4e65de6c56ed0fca67173dd9c
Laura <jorgergrande@gmail.com>
2022-05-10 22:38:45 +0200

    user useraa sh

---
+++ annotate_user.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#SBATCH -J dpu_user
+#SBATCH --cpus-per-task=1
+
+python /home/laura/Laura_intel/Desktop/covid_multianalysis/annotate_user.py $1 $2 $3 $4 $5 $6
---
+++ useraa_annotation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#SBATCH -J dpu_useraa
+#SBATCH --cpus-per-task=1
+
+python /home/laura/Laura_intel/Desktop/covid_multianalysis/useraa_annotation.py $1 $2 $3 $4 $5
