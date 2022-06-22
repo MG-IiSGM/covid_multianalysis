339dbcec1d4bb672de392d175711dff6c5826502
Laura <daniel.penasutrilla98@gmail.com>
2022-06-22 13:45:21 +0200

    update compare covidma

--- covidma.py
+++ covidma.py
@@ -368,11 +368,11 @@ def snp_comparison(name_s, args, logger, output, group_name, out_variant_ivar_di
 
 
     recalibrated_revised_df = revised_df(recalibrated_snp_matrix_intermediate, path_compare, min_freq_include=0.7,
-                                                 min_threshold_discard_sample=0.4, min_threshold_discard_position=0.4, remove_faulty=True, drop_samples=True, drop_positions=True)
+                                                 min_threshold_discard_sample=0.07, min_threshold_discard_position=0.4, remove_faulty=True, drop_samples=True, drop_positions=True)
     # recalibrated_revised_df.to_csv(
     #     compare_snp_matrix_recal, sep="\t", index=False)
     recalibrated_revised_INDEL_df = revised_df(compare_snp_matrix_INDEL_intermediate_df, path_compare, min_freq_include=0.7,
-                                                min_threshold_discard_sample=0.4, min_threshold_discard_position=0.4, remove_faulty=True, drop_samples=True, drop_positions=True)
+                                                min_threshold_discard_sample=0.07, min_threshold_discard_position=0.4, remove_faulty=True, drop_samples=True, drop_positions=True)
     # recalibrated_revised_INDEL_df.to_csv(
     #     compare_snp_matrix_INDEL, sep="\t", index=False)
     
@@ -390,8 +390,8 @@ def snp_comparison(name_s, args, logger, output, group_name, out_variant_ivar_di
     p3.join()
     p4.join()
     
-    p1 = multiprocessing.Process(target=ddtb_compare, args=[compare_snp_matrix_recal], kwargs={"distance":args.distance, "indel":False})
-    p2 = multiprocessing.Process(target=ddtb_compare, args=[compare_snp_matrix_INDEL],  kwargs={"distance":args.distance, "indel":True})
+    p1 = multiprocessing.Process(target=ddtb_compare, args=[compare_snp_matrix_recal], kwargs={"distance":0, "indel":False})
+    p2 = multiprocessing.Process(target=ddtb_compare, args=[compare_snp_matrix_INDEL],  kwargs={"distance":0, "indel":True})
     p1.start()
     p2.start()
     p1.join()
